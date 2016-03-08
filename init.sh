0b6c99bb57f1d31ecf1279781a25bb3dd99a70c8
natalizav <natalizav@mail.ru>
2016-03-08 16:53:40 +0000

    2

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sudo ?ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
