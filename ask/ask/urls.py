8211656e664eadc7d1bb25bef3ab1056762b958a
natalizav <natalizav@mail.ru>
2016-03-13 17:58:28 +0000

    djngo completed

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns['',
-   	url(r'^$', 'qa.views.test', name='home'),
-	url(r'^login/$', 'qa.views.test', name='login'),
-	url(r'^signup/$', 'qa.views.test', name='signup'),
-	url(r'^/question/(\d+)/$', 'qa.views.test', name='question'),
-	url(r'^ask/$', 'qa.views.test', name='ask'),
-	url(r'^popular/$', 'qa.views.test', name='popular'),
-	url(r'^new/$', 'qa.views.test', name='new'),
-]
+urlpatterns = patterns('qa.views',
+   	url(r'^$', 'test', name='home'),
+	url(r'^login/$', 'test', name='login'),
+	url(r'^signup/$', 'test', name='signup'),
+	url(r'^/question/(\d+)/$', 'test', name='question'),
+	url(r'^ask/', 'test', name='ask'),
+	url(r'^popular/$', 'test', name='popular'),
+	url(r'^new/$', 'test', name='new'),
+)
 	
--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+git config --global user.email "natalizav@mail.ru"
+git config --global user.name "natalizav"
+
