11ef1791682fd18c83d642536a0dd72e4b497309
natalizav <natalizav@mail.ru>
2016-03-16 23:12:40 +0200

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
 def test (request, *args, **kwargs):
-	return HttpResponse('OK')
+	return HttpResponse
+
+def list_new_questions (request):
+	questions = Question.objects.order_by('-added_at')
+	page = request.GET.get('page', 1)
+	paginator = Paginator (questions, 10)
+	page = paginator.page(page)
+	return render(request,'qa/list_questions', {
+		'questions': page.object_list,
+		'paginator': paginator, 'page': page,
+	})
+	
+def list_popular_questions (request):
+	questions = Question.objects.order_by('-rating')
+	page = request.GET.get('page', 1)
+	paginator = Paginator (questions, 10)
+	page = paginator.page(page)
+	return render(request,'qa/list_questions.html', {
+		'questions': page.object_list,
+		'paginator': paginator, 'page': page,
+	})
+	
+def get_question (request, id):
+	try:
+		question = Question.objects.get(id=id)
+	except Question.DoesNotExist:
+		raise Http404
+	return render(request, 'qa/question.html', {
+		'question': question,
+	})
+
+	
