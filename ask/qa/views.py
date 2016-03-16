21cd65b4a4c4c61c250e37ef00ca0c5dc1768505
natalizav <natalizav@mail.ru>
2016-03-16 23:47:14 +0200

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -11,7 +11,7 @@ def list_new_questions (request):
 	page = request.GET.get('page', 1)
 	paginator = Paginator (questions, 10)
 	page = paginator.page(page)
-	return render(request,'qa/list_questions', {
+	return render(request, 'list_questions.html', {
 		'questions': page.object_list,
 		'paginator': paginator, 'page': page,
 	})
@@ -21,7 +21,7 @@ def list_popular_questions (request):
 	page = request.GET.get('page', 1)
 	paginator = Paginator (questions, 10)
 	page = paginator.page(page)
-	return render(request,'qa/list_questions.html', {
+	return render(request, 'list_questions.html', {
 		'questions': page.object_list,
 		'paginator': paginator, 'page': page,
 	})
@@ -31,7 +31,7 @@ def get_question (request, id):
 		question = Question.objects.get(id=id)
 	except Question.DoesNotExist:
 		raise Http404
-	return render(request, 'qa/question.html', {
+	return render(request, 'question.html', {
 		'question': question,
 	})
 
